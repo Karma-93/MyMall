ff2aa4c18d025f1d7840b194086a20fd2bef8766
Alone-93 <409433628@qq.com>
2019-11-21 09:30:50 +0800

    完善商品管理接口

--- src/main/java/com/alone/mymall/service/impl/PmsProductAttributeServiceImpl.java
+++ src/main/java/com/alone/mymall/service/impl/PmsProductAttributeServiceImpl.java
@@ -1,38 +1,54 @@
 package com.alone.mymall.service.impl;
 
+import com.alone.mymall.mgb.mapper.PmsProductAttributeMapper;
 import com.alone.mymall.mgb.model.PmsProductAttribute;
+import com.alone.mymall.mgb.model.PmsProductAttributeExample;
 import com.alone.mymall.service.PmsProductAttributeService;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PmsProductAttributeServiceImpl implements PmsProductAttributeService {
+
+    @Autowired
+    private PmsProductAttributeMapper pmsProductAttributeMapper;
     @Override
     public int create(PmsProductAttribute pmsProductAttribute) {
-        return 0;
+        return pmsProductAttributeMapper.insertSelective(pmsProductAttribute);
     }
 
     @Override
     public List<PmsProductAttribute> getList(Long cid, Integer type, Integer pageSize, Integer pageNum) {
-        return null;
+        PageHelper.startPage(pageNum,pageSize);
+        PmsProductAttributeExample pmsProductAttributeExample=new PmsProductAttributeExample();
+        pmsProductAttributeExample.setOrderByClause("sort desc");
+        pmsProductAttributeExample.createCriteria().andProductAttributeCategoryIdEqualTo(cid).andTypeEqualTo(type);
+        return pmsProductAttributeMapper.selectByExample(pmsProductAttributeExample);
     }
 
     @Override
     public List<PmsProductAttribute> listAll(Integer pageSize, Integer pageNum) {
-        return null;
+        PageHelper.startPage(pageNum,pageSize);
+        return pmsProductAttributeMapper.selectByExample(new PmsProductAttributeExample());
     }
 
     @Override
     public PmsProductAttribute getProductAttribute(Long id) {
-        return null;
+        return pmsProductAttributeMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public int deleteBatch(List<Long> ids) {
-        return 0;
+        PmsProductAttributeExample pmsProductAttributeExample=new PmsProductAttributeExample();
+        pmsProductAttributeExample.createCriteria().andIdIn(ids);
+        return pmsProductAttributeMapper.deleteByExample(pmsProductAttributeExample);
     }
 
     @Override
     public int update(PmsProductAttribute pmsProductAttribute) {
-        return 0;
+        return pmsProductAttributeMapper.updateByPrimaryKeySelective(pmsProductAttribute);
     }
 }
--- src/main/java/com/alone/mymall/service/impl/PmsProductServiceImpl.java
+++ src/main/java/com/alone/mymall/service/impl/PmsProductServiceImpl.java
@@ -11,7 +11,10 @@ import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BEncoderStream;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import javax.swing.plaf.metal.MetalToggleButtonUI;
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -25,9 +28,16 @@ public class PmsProductServiceImpl implements PmsProductService {
 
     @Override
     public int create(PmsProductParam pmsProductParam) {
-        PmsProduct pmsProduct=new PmsProduct();
-        BeanUtils.copyProperties(pmsProductParam,pmsProduct);
-        return pmsProductMapper.insertSelective(pmsProduct);
+        int count;
+        //创建商品
+        PmsProduct pmsProduct=pmsProductParam;
+        pmsProduct.setId(null);
+        pmsProductMapper.insertSelective(pmsProduct);
+        //根据促销类型设置价格：/阶梯价格、满减价格
+        Long productId=pmsProduct.getId();
+        //会员价格
+
+        return count;
     }
 
     @Override
@@ -82,5 +92,25 @@ public class PmsProductServiceImpl implements PmsProductService {
     @Override
     public List<PmsProduct> list(String keyword) {
         return null;
+    }
+
+
+    /**
+     * 建立插入表关系操作
+     * @param dao       可以操作的Dao
+     * @param dataList  要插入的数据
+     * @param productId 建立关系的ID
+     */
+    private void relateAndInsertList(Object dao,List dataList,Long productId){
+        try {
+            if(CollectionUtils.isEmpty(dataList)) return;
+            for(Object item:dataList){
+                Method setId=item.getClass().getMethod("setId",Long.class);
+                setId.invoke(item,(Long)null);
+
+            }
+        }catch (Exception e){
+
+        }
     }
 }
