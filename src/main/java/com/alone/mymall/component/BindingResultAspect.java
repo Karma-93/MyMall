ba3c7108ff6c1cc4fcefa61a07e1ad948be0a64f
Alone-93 <409433628@qq.com>
2019-12-05 09:36:47 +0800

    “”

---
+++ src/main/java/com/alone/mymall/component/BindingResultAspect.java
@@ -0,0 +1,41 @@
+package com.alone.mymall.component;
+
+import com.alone.mymall.common.api.CommonResult;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Aspect
+@Component
+@Order(2)
+/**
+ * HibernateValidator错误结果处理切面
+ */
+public class BindingResultAspect{
+    @Pointcut("execution(public * com.alone.mymall.controller.*.*(..))")
+    public void BindingResultAspect(){
+
+    }
+    public Object doAround(ProceedingJoinPoint joinPoint)throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof BindingResult) {
+                BindingResult result = (BindingResult) arg;
+                if (result.hasErrors()) {
+                    FieldError fieldError = result.getFieldError();
+                    if(fieldError!=null){
+                        return CommonResult.validateFailed(fieldError.getDefaultMessage());
+                    }else{
+                        return CommonResult.validateFailed();
+                    }
+                }
+            }
+        }
+        return joinPoint.proceed();
+    }
+}
