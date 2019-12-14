44da2ea40b701d6dc69406f537155cda5225859f
Alone-93 <409433628@qq.com>
2019-12-14 18:37:53 +0800

    添加用户管理接口

--- src/main/java/com/alone/mymall/config/SucrityConfig.java
+++ src/main/java/com/alone/mymall/config/SucrityConfig.java
@@ -25,7 +25,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
 import java.util.List;
 
 /**
@@ -47,62 +46,54 @@ public class SucrityConfig extends WebSecurityConfigurerAdapter {
      * @param httpSecurity
      */
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf()
-        .disable()
-        .sessionManagement()//基于token，所以不需要session
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
-        .antMatchers(HttpMethod.GET,//允许对网站的静态资源的无权限访问
-        "/",
-        "/*.html",
-        "/favicon.ico",
-        "/**/*.html",
-        "/**/*.css",
-        "/**/*.js",
-        "/swagger-resources/**",
-        "/v2/api-docs/**",
-        "/webjars/springfox-swagger-ui/**"
-        )
-        .permitAll()
-        .antMatchers("/admin/login","/admin/register")//对登录注册允许匿名访问
-        .permitAll()
-        .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf()// 由于使用的是JWT，我们这里不需要csrf
+                .disable()
+                .sessionManagement()// 基于token，所以不需要session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, // 允许对于网站静态资源的无授权访问
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/swagger-resources/**",
+                        "/v2/api-docs/**",
+                        "/webjars/springfox-swagger-ui/**"
+                )
                 .permitAll()
-         //       .antMatchers("/**")//测试时全部运行访问
-           //     .permitAll()
+                .antMatchers("/admin/login", "/admin/register")// 对登录注册要允许匿名访问
+                .permitAll()
+                .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次options请求
+                .permitAll()
+//                .antMatchers("/**")//测试时全部运行访问
+//                .permitAll()
                 .anyRequest()// 除上面外的所有请求全部需要鉴权认证
                 .authenticated();
-        //禁用缓存
+        // 禁用缓存
         httpSecurity.headers().cacheControl();
-        //添加JWT的Filter
+        // 添加JWT filter
         httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        //添加自定义未授权和未登录的返回结果
-        httpSecurity.exceptionHandling().accessDeniedHandler(restfulAccessDeniedHandler)//restFulAccessDeiedhandler用户没有访问权限时的处理器，用于返回处理过的JSON
-                                        .authenticationEntryPoint
-                                        (restAuthenticationEntryPoint);//restAuthenticationEntryPoint当未登录或token过期时，返回JSON格式的结果
+        //添加自定义未授权和未登录结果返回
+        httpSecurity.exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                .authenticationEntryPoint(restAuthenticationEntryPoint);
     }
 
-    /**
-     * 用来配置UserDetailsService及passwordEcoder
-     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService())
                 .passwordEncoder(passwordEncoder());
     }
 
-
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * SpringSucrity定义的核心接口，用于根据用户名获取用户信息，需要实现
-     */
     @Bean
     public UserDetailsService userDetailsService() {
         //获取登录用户信息
@@ -115,8 +106,9 @@ public class SucrityConfig extends WebSecurityConfigurerAdapter {
             throw new UsernameNotFoundException("用户名或密码错误");
         };
     }
+
     @Bean
-    public Filter jwtAuthenticationTokenFilter(){
+    public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter(){
         return new JwtAuthenticationTokenFilter();
     }
 
--- src/main/java/com/alone/mymall/pojo/AdminUserDetails.java
+++ src/main/java/com/alone/mymall/pojo/AdminUserDetails.java
@@ -1,15 +1,14 @@
 package com.alone.mymall.pojo;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.alone.mymall.mgb.model.UmsAdmin;
 import com.alone.mymall.mgb.model.UmsPermission;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * springSucrity需要的用户信息
@@ -18,54 +17,47 @@ public class AdminUserDetails implements UserDetails {
 
     private UmsAdmin umsAdmin;
     private List<UmsPermission> permissionList;
-
-    public AdminUserDetails(UmsAdmin umsAdmin,List<UmsPermission> permissions){
-        this.umsAdmin=umsAdmin;
-        this.permissionList=permissions;
+    public AdminUserDetails(UmsAdmin umsAdmin,List<UmsPermission> permissionList) {
+        this.umsAdmin = umsAdmin;
+        this.permissionList = permissionList;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         //返回当前用户的权限
-         return permissionList.stream()
-                        .filter(permission -> permission.getValue()!=null)
-                        .map(permission ->new SimpleGrantedAuthority(permission.getValue()))
-                       .collect(Collectors.toList());
+        return permissionList.stream()
+                .filter(permission -> permission.getValue()!=null)
+                .map(permission ->new SimpleGrantedAuthority(permission.getValue()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
         return umsAdmin.getPassword();
     }
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
         return umsAdmin.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return umsAdmin.getStatus().equals(1);
     }
 
